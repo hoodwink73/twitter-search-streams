00ad5b60e1ad600460475734ece45b2e5b3554ef
Arijit Bhattacharya <hoodwink73@live.com>
2016-07-12 12:16:18 +0530

    Normalize the response data using a schema
    We don not yet use the normalized in our state. This commit just defines a schema for normalization and logs the normalized data

--- src/routes/TwitterSearchStream/modules/actions.js
+++ src/routes/TwitterSearchStream/modules/actions.js
@@ -1,4 +1,5 @@
 import { CALL_API } from 'redux-api-middleware'
+import { Schema, arrayOf, normalize } from 'normalizr'
 
 export const ADD_STREAM = 'ADD_STREAM'
 export const REQUEST_STREAM = 'REQUEST_STREAM'
@@ -7,6 +8,30 @@ export const FAILED_STREAM = 'FAILED_STREAM'
 export const REMOVE_STREAM = 'REMOVE_STREAM'
 
 const twitterStreamAPI = (keyword) => `/api/search/${keyword}`
+
+const streamSchema = new Schema('streams', {
+  idAttribute: function (rawAPI) {
+    // rawAPI denotes exactly what twitter responds like
+    return rawAPI.search_metadata.query
+  },
+  assignEntity (output, key, value, input) {
+    // we delete search_metadata from stream
+    if (key === 'search_metadata') {
+      delete output.search_metadata
+    } else if (key === 'statuses') {
+      // rename 'statuses' key to 'tweets'
+      // after making it a part of stream
+      output.tweets = output.statuses
+      delete output.statuses
+    }
+  }
+})
+
+const tweetSchema = new Schema('tweets', 'id_str')
+
+streamSchema.define({
+  tweets: arrayOf(tweetSchema)
+})
 
 export function addStream (keyword) {
   return {
@@ -36,10 +61,13 @@ export function fetchStream (keyword) {
       {
         type: RECEIVE_STREAM,
         payload: (action, state, response) => {
-          return response.json().then(response => ({
-            keyword,
-            tweets: response.statuses
-          }))
+          return response.json().then(response => {
+            console.log(normalize(response, streamSchema))
+            return {
+              keyword,
+              tweets: response.statuses
+            }
+          })
         }
       },
         FAILED_STREAM
